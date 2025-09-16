be1b7beb2940a604efba06bd82ae564c40d55aac
PC-1\Manuel <manuel.cabrera@fisegroup.com>
2025-09-16 10:58:53 -0500

    Reapply "no message"
    This reverts commit f64e1f1c20770182b5a4329026abab36f137de2d.

--- src/Infrastructure/Data/Repositories/Billing/InvoiceRepository.cs
+++ src/Infrastructure/Data/Repositories/Billing/InvoiceRepository.cs
@@ -79,7 +79,7 @@ namespace InventarioBackend.Infrastructure.Data.Repositories.Billing
         public async Task<List<Invoice>> GetInvoicesByDateAsync(DateTime date, Guid entitiId)
         {
             return await _context.Invoices
-                .Where(invoice => invoice.CreatedAt.Date == date.Date && invoice.EntitiId == entitiId)
+                .Where(invoice => invoice.CreatedAt.Date == date.Date && invoice.EntitiId == entitiId && invoice.isCancelled == false)
                 .ToListAsync();
         }
 
